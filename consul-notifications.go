a060797e0ccafa40f06a8c1e43de1b2d00983363
David Pires <david.pires@gmail.com>
2015-12-20 20:07:36 -0500

    Refactoring ConsulNotifications outside of main

--- consul-notifications.go
+++ consul-notifications.go
@@ -10,29 +10,46 @@ import (
 	"github.com/hashicorp/consul/api"
 )
 
-func main() {
-	config := api.DefaultConfig()
-	consulClient, _ := api.NewClient(config)
+type ConsulNotifications struct {
+    ConsulClient election.ConsulInterface
+    Leader *election.LeaderElection
+}
 
-	leaderElection := &election.LeaderElection{
-		StopElection:  make(chan bool),
-		LeaderKey:     "service/consul-notifications/leader",
-		WatchWaitTime: 3,
-		Client: &client.ConsulClient{
-			Client: consulClient,
-		},
-	}
+func (cn *ConsulNotifications) Start() {
 
-	go leaderElection.ElectLeader()
+	go cn.Leader.ElectLeader()
 
-	mon := monitors.StartMonitor(make(chan bool), leaderElection)
+	mon := monitors.StartMonitor(make(chan bool), cn.Leader)
 
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, os.Interrupt, os.Kill)
 	select {
 	case <-signalChannel:
 		mon.StopMonitorChannel <- true
-		leaderElection.CancelElection()
+		cn.Leader.CancelElection()
 		log.Info("Exiting Consul Notifications")
 	}
+}
+
+func main() {
+	config := api.DefaultConfig()
+	consulClient, _ := api.NewClient(config)
+
+        consulInterface := &client.ConsulClient{
+                Client: consulClient,
+        }
+
+	leaderElection := &election.LeaderElection{
+		StopElection:  make(chan bool),
+		LeaderKey:     "service/consul-notifications/leader",
+		WatchWaitTime: 3,
+                Client: consulInterface,
+	}
+
+        app := &ConsulNotifications{
+            ConsulClient: consulInterface,
+            Leader: leaderElection,
+        } 
+
+        app.Start()
 }
