a66284878fccf049a7e59753bb453e35ac2d807d
David Pires <david.pires@gmail.com>
2016-01-03 14:38:10 -0500

    Renaming executable directory

---
+++ cmd/consul-notifications_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	. "github.com/franela/goblin"
+	"testing"
+)
+
+func TestConsulNotifications(t *testing.T) {
+	g := Goblin(t)
+	g.Describe("Consul Notifications", func() {
+
+	})
+}
